70fdd6d22cf0595f429e388d1d052b74a03258a4
llars <llars@DESKTOP-CC0JRRI>
2017-04-17 19:01:53 +0300

    hello

---
+++ src/Lior1/Lior1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lior1;
+
+/**
+ *
+ * @author moshe
+ */
+public class Lior1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String S;
+        S="I_Hate+GitHub1";
+        System.out.println(S);
+        // TODO code application logic here
+    }
+    
+}
