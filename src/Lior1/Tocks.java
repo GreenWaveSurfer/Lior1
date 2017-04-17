94a8aeb3c09fcae49c42f3ab19e9ef1acce99b60
llars <llars@DESKTOP-CC0JRRI>
2017-04-18 01:03:31 +0300

    Tocks

---
+++ src/Lior1/Tocks.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lior1;
+import java.util.*;
+import java.io.*;
+public class Tocks {
+    private int TocksNumber;
+    private String TocksName;
+    private Vector<Date> Day;
+    private Vector<Double> CloseRate2, ChangeRate3,OpenRate4,BaseRate5,HighRate6,LowRate7,TotalValueForTrade8,
+            MarketCup9,TradeValNIS10, TradeValUnit11,TradeValDeals12;
+    
+    public Tocks() {
+        //Create here the Contstractor that Will read from the CSV file
+    }
+}
